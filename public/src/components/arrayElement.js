f41598d7d3e30a6e2da23fdf1164ec588bf6da7c
MSU <m.stura@castsoftware.com>
2018-07-19 14:45:52 +0200

    sync

--- public/src/components/arrayElement.js
+++ public/src/components/arrayElement.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import { createClassName } from '../common/lib';
 import ArrayCell from './arrayCell';
 
 export default class ArrayElement extends React.PureComponent{
   render(){
     return (
-      <tr onClick={() =>this.props.onItemClick(this.props.values)}>
+      <tr onClick={this.props.onItemClick}>
         {this.props.children.map( val => <ArrayCell header={this.props.header} critical={val.critical}>{val.name}</ArrayCell> )}
       </tr>
     );
